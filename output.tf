c1db69e6097175b56b2721604c3092a0737b8645
Cesar Diaz <cesar@cesardiaz.me>
2021-11-16 19:36:55 -0500

    added private ip to output

--- output.tf
+++ output.tf
@@ -1,4 +1,8 @@
 output "public_IP" {
   value = azurerm_windows_virtual_machine.diaz-win-01.public_ip_address
 }
+output "private_ip" {
+  value = azurerm_windows_virtual_machine.diaz-win-01.private_ip_address
+  
+}
 
